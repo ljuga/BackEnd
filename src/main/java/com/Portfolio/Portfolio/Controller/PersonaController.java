fa09d8578fa71d07a151d90833f4634963941c70
ljuga <ljuga8@gmail.com>
2023-01-06 14:25:38 -0300

    fin

--- src/main/java/com/Portfolio/Portfolio/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/Portfolio/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,7 +43,7 @@ public class PersonaController {
         return new ResponseEntity(persona, HttpStatus.OK);
     }
 
-    /* @PostMapping("/create")
+     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody DtoPersona dtoPers) {
         if (StringUtils.isBlank(dtoPers.getNombre())) {
             return new ResponseEntity(new Mensaje("El nombre es Obligatorio"), HttpStatus.BAD_REQUEST);
@@ -57,7 +58,7 @@ public class PersonaController {
         sPersona.save(persona);
 
         return new ResponseEntity(new Mensaje("Persona Agregada"), HttpStatus.OK);
-    }*/
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DtoPersona dtoPers) {
