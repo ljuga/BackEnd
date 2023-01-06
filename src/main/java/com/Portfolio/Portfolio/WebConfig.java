f2aa03c3893c55faa788fd26e57960af2218ac18
ljuga <ljuga8@gmail.com>
2023-01-06 12:41:33 -0300

    a

--- src/main/java/com/Portfolio/Portfolio/WebConfig.java
+++ src/main/java/com/Portfolio/Portfolio/WebConfig.java
@@ -6,11 +6,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
-
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
 
     public void addCorsMappings(CorsRegistry registry) {
